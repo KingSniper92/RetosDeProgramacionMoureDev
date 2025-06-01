67d3dfae5a05aeadd1c6588544e580472c87dbfb
KingSniper92 <triple.hch@gmail.com>
2025-06-01 12:28:23 -0500

    Initial commit

---
+++ src/CeroSintaxisVariable.java
@@ -0,0 +1,24 @@
+public class CeroSintaxisVariable {
+    public static void main(String[] args) {
+
+      //https://www.java.com/es/
+      /*Esta es otra manera de generar o crear un comentario*/
+
+        String texto = "Variable del tipo String";
+        final String MESENCURSO = "Junio";
+
+        byte datoUno = 1; // Entero de 8 bits
+        short datoDos = 2; // Entero de 16 bits
+        int datoTres = 3; // Entero de 32 bits
+        long datoCuatro = 4; // Entero de 64 bits
+        float datocinco = 5; // Decimal de 32 bits poco preciso
+        double datoSeis = 6.1; // Decimal de 64 bits mas preciso
+        char DatoLetra = 'A'; // Dato de caracter individual
+        boolean DatoLogico = true; // Dato logico de true o false
+
+
+        System.out.printf("¡Hola, JAVA!");
+
+
+    }
+}
